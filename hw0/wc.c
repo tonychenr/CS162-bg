0e6eb6c3b2ef81643fda9972cdfdbe03a25bd4ef
Tony Chen <tonychenr@gmail.com>
2015-09-05 23:41:53 +0000

    Finished my first CS162 assignment

--- hw0/wc.c
+++ hw0/wc.c
@@ -1,3 +1,50 @@
-int main(int argc, char *argv[]) {
-    return 0;
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+int wc(FILE *readfile, int *counts) {
+  int charcount = 0;
+  int wordcount = 0;
+  int linecount = 0;
+  int curr = fgetc(readfile);
+  int prev = fgetc(readfile);
+  while (1) {
+    charcount++;
+    if (curr == EOF) {
+      if (!isspace(prev)) {
+        wordcount++;
+      }
+      break;
+    } else if (isspace(curr)) {
+      if (!isspace(prev)) {
+        wordcount++;
+      }
+      if (curr == '\n') {
+        linecount++;
+      }
+    }
+    prev = curr;
+    curr = fgetc(readfile);
+  }
+  counts[0] = linecount;
+  counts[1] = wordcount;
+  counts[2] = charcount;
 }
+
+int main(int argc, char *argv[]) {
+  int *counts = (int *)malloc(3*sizeof(int));
+  if (argc == 1) {
+    wc(stdin, counts);
+    printf("%d %d %d\n", counts[0], counts[1], counts[2]);
+  } else {
+    for (int i = 1; i < argc; i++){
+      FILE *input = fopen(argv[i], "r");
+      wc(input, counts);
+      printf("%d %d %d %s\n", counts[0], counts[1], counts[2], argv[i]);
+      fclose(input);
+    }
+  }
+
+  return 0;
+}
+
