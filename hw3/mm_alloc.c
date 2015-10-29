65da1fd8ba070b7bdb73d996eecdbbdde5023863
Tony Chen <tonychenr@gmail.com>
2015-10-29 07:48:47 +0000

    Initial hw3

--- hw3/mm_alloc.c
+++ hw3/mm_alloc.c
@@ -6,17 +6,94 @@
 
 #include "mm_alloc.h"
 #include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+struct list *mem_chunks = NULL;
+
+void mem_init(struct list* list) {
+    if (list == NULL) {
+        mem_chunks = sbrk(sizeof(struct list));
+        list_init(mem_chunks);
+    }
+}
 
 void *mm_malloc(size_t size) {
     /* YOUR CODE HERE */
-    return NULL;
+    mem_init(mem_chunks);
+    if (size <= 0) {
+        return NULL;
+    } else {
+        struct list_elem *nextChunk = getNextAvailableChunk(mem_chunks, size);
+        if (nextChunk == NULL) {
+            void* oldbrk = sbrk(size + sizeof(struct list_elem));
+            if (oldbrk == (void *) -1) {
+                return NULL;
+            }
+            struct list_elem *newChunk;
+            newChunk = oldbrk;
+            newChunk->isFree = false;
+            newChunk->size = size;
+            list_insert_ascending(mem_chunks, newChunk);
+            nextChunk = newChunk;
+            return nextChunk->data;
+        } else {
+            if (nextChunk->size > size + sizeof(struct list_elem)) {
+                nextChunk->size = nextChunk->size - size - sizeof(struct list_elem);
+                struct list_elem *newChunk = nextChunk + sizeof(struct list_elem) + nextChunk->size;
+                newChunk->isFree = false;
+                newChunk->size = size;
+                list_insert_ascending(mem_chunks, newChunk);
+                return newChunk->data;
+            } else {
+                nextChunk->isFree = false;
+                return nextChunk->data;
+            }
+        }
+    }
 }
 
 void *mm_realloc(void *ptr, size_t size) {
     /* YOUR CODE HERE */
+    if (ptr == NULL) {
+        return mm_malloc(size);
+    }
+
+    mem_init(mem_chunks);
+    struct list_elem *oldChunk = getChunk(mem_chunks, ptr);
+    if (oldChunk != NULL && oldChunk->size >= size) {
+        return oldChunk->data;
+    }
+
+    void *newBlock = mm_malloc(size);
+    if (newBlock != NULL) {
+        memcpy(newBlock, oldChunk->data, size);
+        mm_free(ptr);
+        return newBlock;
+    }
+
     return NULL;
 }
 
 void mm_free(void *ptr) {
     /* YOUR CODE HERE */
+    mem_init(mem_chunks);
+    if (ptr != NULL) {
+        struct list_elem *chunk = getChunk(mem_chunks, ptr);
+        if (chunk == NULL) {
+            return;
+        }
+        struct list_elem *prevChunk = chunk->prev;
+        struct list_elem *nextChunk = chunk->next;
+        if (chunk != list_begin(mem_chunks) && prevChunk->isFree == true) {
+            prevChunk->size += sizeof(struct list_elem) + chunk->size;
+            list_remove(chunk);
+            chunk = prevChunk;
+        }
+
+        if (chunk->next != list_end(mem_chunks) && nextChunk->isFree == true) {
+            chunk->size += sizeof(struct list_elem) + nextChunk->size;
+            list_remove(nextChunk);
+        }
+    }
 }
--- hw3/mm_alloc.h
+++ hw3/mm_alloc.h
@@ -7,7 +7,75 @@
 #pragma once
 
 #include <stdlib.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
 
 void *mm_malloc(size_t size);
 void *mm_realloc(void *ptr, size_t size);
 void mm_free(void *ptr);
+
+struct list_elem {
+    struct list_elem *prev;
+    struct list_elem *next;
+    bool isFree;
+    size_t size;
+    char data[0];
+};
+
+struct list {
+    struct list_elem head;
+    struct list_elem tail;
+};
+
+static void list_init (struct list *list) {
+    list->head.next = &list->tail;
+    list->head.prev = NULL;
+    list->tail.prev = &list->head;
+    list->tail.next = NULL;
+}
+
+static struct list_elem *list_begin(struct list *list) {
+    return list->head.next;
+}
+
+static struct list_elem *list_end(struct list *list) {
+    return &list->tail;
+}
+
+static void list_insert(struct list_elem *before, struct list_elem *elem) {
+    elem->prev = before->prev;
+    elem->next = before;
+    before->prev->next = elem;
+    before->prev = elem;
+}
+
+static void list_insert_ascending(struct list *list, struct list_elem *elem) {
+    struct list_elem *e;
+    for (e = list_begin (list); e != list_end (list); e = e->next)
+        if (e < elem)
+            break;
+    return list_insert (e, elem);
+}
+
+static void list_remove (struct list_elem *elem) {
+    elem->prev->next = elem->next;
+    elem->next->prev = elem->prev;
+}
+
+static struct list_elem *getNextAvailableChunk(struct list *list, size_t size) {
+    struct list_elem *e = NULL;
+    for (e = list_begin (list); e != list_end (list); e = e->next)
+        if (e->size >= size && e->isFree == true)
+            return e;
+    return NULL;
+}
+
+static struct list_elem *getChunk(struct list *list, void *ptr) {
+    struct list_elem *e = NULL;
+    for (e = list_begin (list); e != list_end (list); e = e->next)
+        if (e->data == ptr)
+            return e;
+    return NULL;
+}
+
